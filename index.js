ce4559e7387242005eba3268ed0b8023f013044d
Karina Evers <krin_11_bball@yahoo.com>
2019-06-06 21:25:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma (string) {
-  return string.toUpperCase()
+  if (string===lowercase) {
+    return "I can't hear you"  
+  }  
 }
 
 function sayHiToGrandma (string) {
