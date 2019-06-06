76025522b0d904f874d2796b082240df95625265
Karina Evers <krin_11_bball@yahoo.com>
2019-06-06 21:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma (string) {
   if (string.toUpperCase() === string){
   return 'YES INDEED!';
   }
-  if (string === 'I LOVE YOU, GRANDMA'){
+  if (string === 'I love you, Grandma.'){
   return 'I love you, too';
   }
 }
