2b18430003b74ebf772bb1c15fce108136f4e84b
Karina Evers <krin_11_bball@yahoo.com>
2019-06-06 21:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function logShout (string) {
 function logWhisper (string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma (string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma (string) {
+  return string.toUpperCase()
+}
